c8084f63ddaf7b1980b71d1f15225ef57d8b0b98
Tom Dong <translucentleaf@protonmail.com>
2020-02-04 15:17:59 -0600

    Got rid of some boilerplate stuff

---
+++ src/components/app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hi you should really check back on this in like 2 weeks.</h1>
+    </div>
+  );
+}
+
+export default App;
