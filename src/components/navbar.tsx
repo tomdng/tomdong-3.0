57aa3ac76b4d05c8802de4f6b35d7df071839947
Tom Dong <contact@tomdong.io>
2020-02-27 14:17:54 -0600

    Started styling navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,33 +1,37 @@
-import { Link } from 'gatsby';
 import React from 'react';
+import styled, { AnyStyledComponent } from 'styled-components';
+import { Link } from 'gatsby';
 
-const Navbar = ({ siteTitle }): JSX.Element => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+const StyledNavbar: AnyStyledComponent = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #f9f9f9;
+`;
+
+const StyledNavbarContent: AnyStyledComponent = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+`;
+
+const StyledLink: AnyStyledComponent = styled(Link)`
+  text-decoration: none;
+`;
+
+interface NavbarProps {
+  siteTitle: string;
+}
+
+const Navbar = ({ siteTitle }: NavbarProps): JSX.Element => (
+  <StyledNavbar>
+    <StyledNavbarContent>
+      <h1>
+        <StyledLink to="/">{siteTitle}</StyledLink>
       </h1>
-    </div>
-  </header>
+    </StyledNavbarContent>
+  </StyledNavbar>
 );
 
 export default Navbar;
