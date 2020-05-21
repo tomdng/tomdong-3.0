daf1aaebdbf91bafedaf30b54966dd8d600dd7e0
Tom Dong <contact@tomdong.io>
2020-05-21 15:53:53 -0500

    Random accent color on each page

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,8 +1,11 @@
+const colorList = ['#92bc5d', '#7bcbed', '#e392ff'];
+const accentColor = colorList[Math.floor(Math.random() * colorList.length)];
+
 module.exports = {
   white: '#ffffff',
   offWhite: '#f9f9f9',
   grey: '#494949',
-  accent: '#92bc5d',
+  accent: accentColor,
   textPrimary: '#0e0e0e',
   textSecondary: '#232323',
   textPrimaryLight: '#f9f9f9',
